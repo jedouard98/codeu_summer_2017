a7b4becfb062103a1d2468c635ef91e9d3ef79b0
jedouard98 <jedouard98@gmail.com>
2017-08-05 03:04:20 -0400

    Tokenizer v2

--- src/codeu/chat/client/core/ConversationContext.java
+++ src/codeu/chat/client/core/ConversationContext.java
@@ -12,97 +12,88 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package codeu.chat.client.core;
+package com.google.codeu.mathlang.impl;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
+import java.io.IOException;
 
-import codeu.chat.common.BasicController;
-import codeu.chat.common.BasicView;
-import codeu.chat.common.ConversationHeader;
-import codeu.chat.common.ConversationPayload;
-import codeu.chat.common.Message;
-import codeu.chat.common.User;
-import codeu.chat.util.Uuid;
+import com.google.codeu.mathlang.core.tokens.Token;
+import com.google.codeu.mathlang.parsing.TokenReader;
 
-public final class ConversationContext {
+import com.google.codeu.mathlang.core.tokens.NameToken;
+import com.google.codeu.mathlang.core.tokens.NumberToken;
+import com.google.codeu.mathlang.core.tokens.StringToken;
+import com.google.codeu.mathlang.core.tokens.SymbolToken;
 
-  public final User user;
-  public final ConversationHeader conversation;
+import java.util.HashSet;
 
-  private final BasicView view;
-  private final BasicController controller;
+// MY TOKEN READER
+//
+// This is YOUR implementation of the token reader interface. To know how
+// it should work, read src/com/google/codeu/mathlang/parsing/TokenReader.java.
+// You should not need to change any other files to get your token reader to
+// work with the test of the system.
+public final class MyTokenReader implements TokenReader {
+  public String source;
 
-  public ConversationContext(User user,
-                             ConversationHeader conversation,
-                             BasicView view,
-                             BasicController controller) {
+  private int cursorPosition;
+  private boolean iteratingThroughNote;
+  private boolean noteFlag;
+  private StringBuilder sb;
 
-    this.user = user;
-    this.conversation = conversation;
-    this.view = view;
-    this.controller = controller;
+  public MyTokenReader(String source) {
+    // Your token reader will only be given a string for input. The string will
+    // contain the whole source (0 or more lines).
+
+    this.source = new String(source);
+    this.cursorPosition = 0;
+    this.sb = new StringBuilder();
   }
 
-  public int togglePermission(Uuid userToBeChanged, int permission, Uuid conversation) {
-    return controller.togglePermission(user.id, userToBeChanged, permission, conversation);
-  }
+  @Override
+  public Token next() throws IOException {
+    // Most of your work will take place here. For every call to |next| you should
+    // return a token until you reach the end. When there are no more tokens, you
+    // should return |null| to signal the end of input.
 
-  public MessageContext add(String messageBody) {
+    // If for any reason you detect an error in the input, you may throw an IOException
+    // which will stop all execution.
+    sb.setLength(0);
+    for (int i = cursorPosition; i < source.length(); i++) {
+      char c = source.charAt(i);
+      if (c == '\"') {
+        iteratingThroughNote = true;
+      }
+      else if (endOfTokenCondition(c)) {
+        cursorPosition = i;
+        interpret(sb.toString());
 
-    final Message message = controller.newMessage(user.id,
-                                                  conversation.id,
-                                                  messageBody);
-
-    return message == null ?
-        null :
-        new MessageContext(message, view);
-  }
-
-  public MessageContext firstMessage() {
-    // As it is possible for the conversation to have been updated, so fetch
-    // a new copy.
-    final ConversationPayload updated = getUpdated();
-
-    return updated == null ?
-        null :
-        getMessage(updated.firstMessage);
-  }
-
-  public String getStatus() {
-    final ConversationPayload updated = getUpdated();
-    return updated.getErrorMessage();
-  }
-
-  public MessageContext lastMessage() {
-
-    // As it is possible for the conversation to have been updated, so fetch
-    // a new copy.
-    final ConversationPayload updated = getUpdated();
-
-    return updated == null ?
-        null :
-        getMessage(updated.lastMessage);
-  }
-
-  private ConversationPayload getUpdated() {
-    final Collection<Uuid> ids = Arrays.asList(conversation.id);
-    Collection<ConversationPayload> conversations = null;
-    Iterator<ConversationPayload> payloads = null;
-    try {
-      conversations = view.getConversationPayloads(ids, user.id, conversation.id);
-      payloads =conversations.iterator();
+      }
+      else if (!(Character.isWhitespace(c)))
+        sb.append(source.charAt(c));
     }
-    catch (Exception e) {
-      e.printStackTrace(System.out);
-      return null;
-    }
-    return payloads.hasNext() ? payloads.next() : null;
+
+    return null;
   }
 
-  private MessageContext getMessage(Uuid id) {
-    final Iterator<Message> messages = view.getMessages(Arrays.asList(id)).iterator();
-    return messages.hasNext() ? new MessageContext(messages.next(), view) : null;
+  private Token interpretToken(String token) {
+    if (isSymbol(token))
+      return SymbolToken(token.charAt(0));
+    if (Character.isDigit(token))
+      return NumberToken(Double.parseDouble(token));
+    if (iteratingThroughNote) {
+      iteratingThroughNote = false;
+      return StringToken(token);
+    }
+    else {
+      return NameToken(token);
+    }
+  }
+  private boolean isSymbol(char c) {
+    return (c == '+' || c == '-');
+  }
+
+  private boolean endOfTokenCondition(char c) {
+    if (iteratingThroughNote && c != '\"') return false;
+    return ((Character.isWhitespace(c) && sb.length() > 0) || (c == ';'));
   }
 }
